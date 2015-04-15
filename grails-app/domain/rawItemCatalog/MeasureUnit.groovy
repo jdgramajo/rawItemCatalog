5c3d2bfe080ee4e5ae3311f9e70f8c7b3a66f4f8
diego <juan.gramajo@xoom.com>
2015-04-14 22:43:41 -0600

    Adds MeasureUnit as a resource and also populates dev data from the Bootstrap class, work in progress.

--- grails-app/domain/rawItemCatalog/MeasureUnit.groovy
+++ grails-app/domain/rawItemCatalog/MeasureUnit.groovy
@@ -1,8 +1,12 @@
 package rawItemCatalog
 
+import grails.rest.Resource
+
+@Resource(uri='/MeasureUnit', formats = ['json', 'xml'])
 class MeasureUnit {
 
     String name
+    String abreviation
     String comments
 
     static constraints = {
--- grails-app/init/BootStrap.groovy
+++ grails-app/init/BootStrap.groovy
@@ -1,6 +1,25 @@
+import grails.util.Environment
+import rawItemCatalog.MeasureUnit
+
 class BootStrap {
 
     def init = { servletContext ->
+        switch (Environment.current) {
+            case Environment.DEVELOPMENT:
+                //populate with dev data
+                new MeasureUnit(name: "Grams", abreviation: "grs", comments: "---").save()
+                new MeasureUnit(name: "Pounds", abreviation: "lbs", comments: "---").save()
+                new MeasureUnit(name: "Litter", abreviation: "L", comments: "---").save()
+                new MeasureUnit(name: "Gallon", abreviation: "Gal", comments: "---").save()
+                new MeasureUnit(name: "6 pack", abreviation: "6pck", comments: "---").save()
+                new MeasureUnit(name: "Unit", abreviation: "u", comments: "---").save()
+                new MeasureUnit(name: "Ounces", abreviation: "oz", comments: "---").save()
+                new MeasureUnit(name: "Dozen", abreviation: "dzn", comments: "---").save()
+                new MeasureUnit(name: "Bottle", abreviation: "btl", comments: "---").save()
+                break
+            default:
+                break
+        }
     }
     def destroy = {
     }
